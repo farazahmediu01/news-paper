523fe8213c662027969275b8423ccabd19937607
= <=>
2025-12-30 20:57:00 +0500

    admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "age",
+        "is_staff",
+    ]
+    # add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("age",)}),)
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "age",
+                    "password1",
+                    "password2",
+                ),  # Add confirm_password if it's in your Form
+            },
+        ),
+    )
+    # fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("age",)}),)
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal info",
+            {"fields": ("first_name", "last_name", "email", "age")},
+        ),  # Added age here
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+# username, password, f_name, l_name, age
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True) # optional
